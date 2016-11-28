d924c8885ef96d8b82824851537d91a82901d6eb
ryokuhei <r.sato@solt-inc.com>
2016-11-28 23:44:50 +0900

    Wicket Test Program

---
+++ test_hello/src/main/java/com/solt/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.solt;
+
+import org.apache.wicket.protocol.http.WebApplication;
+public class HelloWorldApplication extends WebApplication {
+    public HelloWorldApplication() {
+    }
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class getHomePage() {
+        return HelloWorld.class;
+    }
+}
